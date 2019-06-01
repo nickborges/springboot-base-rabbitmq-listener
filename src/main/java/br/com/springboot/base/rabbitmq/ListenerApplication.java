00cae28dfaaf95f8aca074d2048b71304effc4cb
nickkrasborges <nickkrasborges@gmail.com>
2019-06-01 13:39:38 -0300

    alteração configuração listener

--- src/main/java/br/com/springboot/base/rabbitmq/ListenerApplication.java
+++ src/main/java/br/com/springboot/base/rabbitmq/ListenerApplication.java
@@ -1,9 +1,14 @@
 package br.com.springboot.base.rabbitmq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +16,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableRabbit
 public class ListenerApplication {
+
+    @Value("${queue.order.name}")
+    String queueName;
+
+    @Value("${queue.order.exchange}")
+    String topicExchangeName;
+
+    @Value("${queue.order.route}")
+    String queueRoute;
 
     public static void main(String[] args) {
         SpringApplication.run(ListenerApplication.class, args);
@@ -27,4 +41,34 @@ public class ListenerApplication {
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueRoute);
+    }
+
+    /*@Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(ListenerService receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }*/
 }
